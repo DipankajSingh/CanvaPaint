06695fcfbfd4269f12cbf7ee7c96f58345f7590a
Dipankaj Singh <dipankajsingh2@gmail.com>
2023-01-26 12:48:23 +0530

    leeh..

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict'
-let penSize
+let penSize = 5
 const canvas = document.createElement('canvas')
 canvas.height = 300
 canvas.width = 400
@@ -8,48 +8,80 @@ $('.CanvasPaper').appendChild(canvas)
 const ctx = canvas.getContext('2d')
 
 let isDrawing = false
-const lines = [
-    [[46, 45], [76, 35], { color: "red", lineWidth: 5 }],
-    [[94, 85], [74, 9]]
-]
-let lastMouseDownPos = [45, 65]
-let lastMouseUpPos = [76, 89]
-
+// const lines = [
+//     [[46, 45], [76, 35], { color: "red", lineWidth: 5 }],
+//     [[94, 85], [74, 9]]
+// ]
+let lastMouseDownPos = [0, 0]
+let lastMouseUpPos = [0, 0]
+let snapShot
+let SelectedColor = '#73ff00'
+let DrawTool = 'pen'
 const boundingRect = canvas.getBoundingClientRect()
 
-canvas.addEventListener('mousedown', e => {
+document.addEventListener('mousedown', e => {
     lastMouseDownPos = [e.x, e.y]
     isDrawing = true
+    snapShot = ctx.getImageData(0, 0, canvas.width, canvas.height)
+
 })
 
-canvas.addEventListener('mouseup', e => {
+document.addEventListener('mouseup', e => {
     isDrawing = false
     lastMouseUpPos = [e.x, e.y]
-    lines.push([lastMouseDownPos, lastMouseUpPos])
+    // lines.push([lastMouseDownPos, lastMouseUpPos])
 })
 
 canvas.addEventListener('mousemove', e => {
     if (isDrawing) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-        for (let i = 0; i < lines.length; i++) {
-            const line = lines[i];
-            drawLine(...line[0], ...line[1])
+        // for (let i = 0; i < lines.length; i++) {
+        //     const line = lines[i];
+        //     drawLine(...line[0], ...line[1])
+        // }
+
+        switch (DrawTool) {
+            case 'line':
+                ctx.clearRect(0, 0, canvas.width, canvas.height)
+                ctx.putImageData(snapShot, 0, 0)
+                drawLine(lastMouseDownPos[0], lastMouseDownPos[1], e.x, e.y,)
+
+                break;
+            case 'pen':
+                drawPen(e, penSize)
+                break;
+            case 'arc':
+                drawArc(lastMouseDownPos[0], lastMouseDownPos[1], This should be done first)
+                break;
+            default:
+                break;
         }
-        drawLine(lastMouseDownPos[0], lastMouseDownPos[1], e.x, e.y)
-
-
     }
 })
 
+function drawArc(x, y, size) {
+    ctx.beginPath()
+    ctx.arc(x - boundingRect.left, y - boundingRect.top, size, 0, 2 * Math.PI)
+    ctx.strokeStyle = SelectedColor
+    ctx.stroke()
+}
 
+function drawPen(event, size = 3) {
+    ctx.beginPath()
+    ctx.arc(event.x - boundingRect.left, event.y - boundingRect.top, size, 0, 2 * Math.PI)
+    ctx.strokeStyle = SelectedColor
+    ctx.fillStyle = SelectedColor
+    ctx.fill()
+    ctx.stroke()
+}
 function drawLine(x1 = 50, y1 = 67, x2 = 50, y2 = 70) {
     ctx.beginPath()
     ctx.moveTo(x1 - boundingRect.left, y1 - boundingRect.top)
     ctx.lineTo(x2 - boundingRect.left, y2 - boundingRect.top)
+    ctx.strokeStyle = SelectedColor
+    ctx.lineWidth = penSize
     ctx.stroke()
 }
 
-drawLine()
 
 
 
@@ -91,7 +123,7 @@ import { bucket, pen } from "./Assets/icons.js";
 // setting bucket and color to draw
 $('.toolItem')[3].innerHTML = bucket('black')
 $('#colorInput').addEventListener('change', (e) => {
-    activeColor = e.target.value
+    SelectedColor = e.target.value
     $('.toolItem')[3].innerHTML = bucket(e.target.value)
 })
 
@@ -111,3 +143,15 @@ Array.from($('.toolItem')).forEach((elm) => {
         }
     })
 })
+
+// this function will set drawing tool by given ID when clicking on it
+function setDrawTool(elm = 'pen') {
+    $(`#${elm}`).addEventListener('click', () => DrawTool = elm)
+}
+
+setDrawTool()
+setDrawTool('line')
+setDrawTool('arc')
+setDrawTool('triangle')
+setDrawTool('square')
+setDrawTool('msgbox')
